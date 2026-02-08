5539073ceec56e175147984e8980305d12309052
Yeva Krysheminska <yevaluation@gmail.com>
2026-02-08 02:34:37 +0200

    hw8.2

--- app/notes/action/create/page.tsx
+++ app/notes/action/create/page.tsx
@@ -1,17 +1,15 @@
-import css from './CreateNote.module.css';
-import NoteForm from '@/components/NoteForm/NoteForm';
-import type { Metadata } from 'next';
+import css from "./CreateNote.module.css";
+import NoteForm from "@/components/NoteForm/NoteForm";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: 'Create note',
-  description: 'Page for creating new note',
+  title: "Create note",
+  description: "Page for creating new note",
   openGraph: {
-    title: 'Create note',
-    description: 'Page for creating new note',
-    url: '/notes/action/create',
-    images: [
-      { url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg' },
-    ],
+    title: "Create note",
+    description: "Page for creating new note",
+    url: "/notes/action/create",
+    images: [{ url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg" }],
   },
 };
 
@@ -20,7 +18,7 @@ export default function CreateNotePage() {
     <main className={css.main}>
       <div className={css.container}>
         <h1 className={css.title}>Create note</h1>
-        <NoteForm closeModal={() => {}} />
+        <NoteForm />
       </div>
     </main>
   );
